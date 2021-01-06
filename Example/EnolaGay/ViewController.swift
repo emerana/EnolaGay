b538091bb54fcb2819fc8cbd02140a7b29029a00
醉翁之意 <Judy_u@163.com>
2021-01-06 09:11:52 +0800

    优化log函数

--- Example/EnolaGay/ViewController.swift
+++ Example/EnolaGay/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import EnolaGay
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Judy.log("太爽了")
     }
 
     override func didReceiveMemoryWarning() {
