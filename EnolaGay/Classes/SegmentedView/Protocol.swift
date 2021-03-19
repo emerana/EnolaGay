307313f9e2ba9d606c392c6265909f436d6606f3
醉翁之意 <Judy_u@163.com>
2021-03-19 16:08:04 +0800

    优化数据源

--- EnolaGay/Classes/SegmentedView/Protocol.swift
+++ EnolaGay/Classes/SegmentedView/Protocol.swift
@@ -104,6 +104,100 @@ public extension SegmentedViewDelegate {
 }
 
 
+// MARK: 内置可用的数据源
+
+/// 可直接使用的适用于 SegmentedItemTitleModel 类型的数据源
+open class SegmentedTitleDataSource: SegmentedViewDataSource {
+    
+    // MARK: - 协议属性
+    public var itemSpacing: CGFloat = 8
+    public var isItemSpacingAverageEnabled: Bool = true
+    public var isItemWidthZoomEnabled: Bool = false
+    public var selectedAnimationDuration: TimeInterval = 0.25
+
+    // MARK: - 自有属性
+    
+    /// 该 title 数组用于确定 dataSource
+    open var titles = ["猴哥", "青蛙王子", "旺财", "大家好我是王仁洁", "粉红猪", "喜羊羊", "黄焖鸡", "小马哥", "牛魔王", "大象先生", "神龙"]
+
+    /// 真实的 item 宽度 = itemWidth + itemWidthIncrement。
+    open var itemWidthIncrement: CGFloat = 0
+    /// 如果将 SegmentedView 嵌套进 UITableView 的 cell，每次重用的时候，SegmentedView 进行 reloadData 时，会重新计算所有的 title 宽度。所以该应用场景，需要 UITableView 的 cellModel 缓存 titles 的文字宽度，再通过该闭包方法返回给 SegmentedView。
+    open var widthForTitleClosure: ((String)->(CGFloat))?
+
+    /// title普通状态时的字体
+    open var titleNormalFont: UIFont = UIFont.systemFont(ofSize: 15)
+
+    public init() {}
+
+
+    public func segmentedView(registerCellForCollectionViewAt collectionView: UICollectionView) -> String {
+        collectionView.register(SegmentedTitleCell.self, forCellWithReuseIdentifier: "Cell")
+        return "Cell"
+    }
+    
+    public func numberOfItems(in segmentedView: SegmentedView) -> Int { titles.count }
+    
+    public func segmentedView(_ segmentedView: SegmentedView, entityForItem entity: SegmentedItemModel?, entityForItemAt index: Int, selectedIndex: Int) -> SegmentedItemModel {
+        
+        var model = SegmentedItemTitleModel()
+        if entity as? SegmentedItemTitleModel != nil {
+            model = entity as! SegmentedItemTitleModel
+        }
+        
+        
+        model.isItemTransitionEnabled = true
+        model.isSelectedAnimable = false
+        
+        model.title = titles[index]
+        
+        model.titleNumberOfLines = 1
+        model.titleNormalColor = .black
+        model.titleSelectedColor = .red
+        model.titleNormalFont = titleNormalFont
+        model.titleSelectedFont = titleNormalFont
+        
+        model.isTitleZoomEnabled = false
+        
+        model.isTitleStrokeWidthEnabled = false
+        model.isTitleMaskEnabled = false
+        
+        model.titleNormalZoomScale = 1
+        model.titleSelectedZoomScale = 1.2
+        model.titleSelectedStrokeWidth = -2
+        model.titleNormalStrokeWidth = 0
+        // 确定选中的模型
+        if index == selectedIndex {
+            model.titleCurrentColor = .red
+            model.titleCurrentZoomScale = 1.2
+            model.titleCurrentStrokeWidth = -2
+        }else {
+            model.titleCurrentColor = .black
+            model.titleCurrentZoomScale = 1
+            model.titleCurrentStrokeWidth = 0
+        }
+        
+        return model
+    }
+    
+    public func segmentedView(_ segmentedView: SegmentedView, widthForItem entity: SegmentedItemModel) -> CGFloat {
+        
+        return itemWidthIncrement + (entity as! SegmentedItemTitleModel).textWidth
+    }
+    
+    public func segmentedView(_ segmentedView: SegmentedView, widthForItemContent item: SegmentedItemModel) -> CGFloat {
+        let model = item as! SegmentedItemTitleModel
+        if model.isTitleZoomEnabled {
+            return model.textWidth*model.titleCurrentZoomScale
+        } else {
+            return model.textWidth
+        }
+    }
+
+}
+
+
+
 // MARK: extension
 
 
--- Example/EnolaGay/Tests/ViewController.swift
+++ Example/EnolaGay/Tests/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var theButton: JudyBaseButton!
     @IBOutlet weak var segmentedView: SegmentedView!
-    private let segmentedCtrlDataSoruce = SegmentedTestSource()
+    private let segmentedCtrlDataSoruce = SegmentedTitleDataSource()
 
     
     override func viewDidLoad() {
@@ -49,99 +49,6 @@ class ViewController: UIViewController {
     
 }
 
-// 用于测试用例的一个数据源对象，SegmentedTitleDataSource 融合 SegmentedBaseDataSource
-class SegmentedTestSource {
-    
-    // MARK: - 协议属性
-    
-    // MARK: - 自有属性
-    
-    /// 该 title 数组用于确定 dataSource
-    open var titles = ["猴哥", "青蛙王子", "旺财", "大家好我是王仁洁", "粉红猪", "喜羊羊", "黄焖鸡", "小马哥", "牛魔王", "大象先生", "神龙"]
-
-    /// 真实的 item 宽度 = itemWidth + itemWidthIncrement。
-    open var itemWidthIncrement: CGFloat = 0
-    /// 如果将 SegmentedView 嵌套进 UITableView 的 cell，每次重用的时候，SegmentedView 进行 reloadData 时，会重新计算所有的 title 宽度。所以该应用场景，需要 UITableView 的 cellModel 缓存 titles 的文字宽度，再通过该闭包方法返回给 SegmentedView。
-    open var widthForTitleClosure: ((String)->(CGFloat))?
-
-    /// title普通状态时的字体
-    open var titleNormalFont: UIFont = UIFont.systemFont(ofSize: 15)
-
-}
-
-// MARK: - SegmentedViewDataSource
-extension SegmentedTestSource: SegmentedViewDataSource {
-    
-    var itemSpacing: CGFloat { 8 }
-    var isItemSpacingAverageEnabled: Bool { true }
-    var isItemWidthZoomEnabled: Bool { false }
-    var selectedAnimationDuration: TimeInterval { 0.25 }
-
-
-    func segmentedView(registerCellForCollectionViewAt collectionView: UICollectionView) -> String {
-        collectionView.register(SegmentedTitleCell.self, forCellWithReuseIdentifier: "Cell")
-        return "Cell"
-    }
-
-    func numberOfItems(in segmentedView: SegmentedView) -> Int { titles.count }
-    
-    func segmentedView(_ segmentedView: SegmentedView, entityForItem entity: SegmentedItemModel?, entityForItemAt index: Int, selectedIndex: Int) -> SegmentedItemModel {
-    
-        var model = SegmentedItemTitleModel()
-        if entity as? SegmentedItemTitleModel != nil {
-            model = entity as! SegmentedItemTitleModel
-        }
-        
-
-        model.isItemTransitionEnabled = true
-        model.isSelectedAnimable = false
-                
-        model.title = titles[index]
-
-        model.titleNumberOfLines = 1
-        model.titleNormalColor = .black
-        model.titleSelectedColor = .red
-        model.titleNormalFont = titleNormalFont
-        model.titleSelectedFont = titleNormalFont
-        
-        model.isTitleZoomEnabled = false
-        
-        model.isTitleStrokeWidthEnabled = false
-        model.isTitleMaskEnabled = false
-        
-        model.titleNormalZoomScale = 1
-        model.titleSelectedZoomScale = 1.2
-        model.titleSelectedStrokeWidth = -2
-        model.titleNormalStrokeWidth = 0
-        // 确定选中的模型
-        if index == selectedIndex {
-            model.titleCurrentColor = .red
-            model.titleCurrentZoomScale = 1.2
-            model.titleCurrentStrokeWidth = -2
-        }else {
-            model.titleCurrentColor = .black
-            model.titleCurrentZoomScale = 1
-            model.titleCurrentStrokeWidth = 0
-        }
-        
-        return model
-    }
-    
-    func segmentedView(_ segmentedView: SegmentedView, widthForItem entity: SegmentedItemModel) -> CGFloat {
-
-        return itemWidthIncrement + (entity as! SegmentedItemTitleModel).textWidth
-    }
-    
-    func segmentedView(_ segmentedView: SegmentedView, widthForItemContent item: SegmentedItemModel) -> CGFloat {
-        let model = item as! SegmentedItemTitleModel
-        if model.isTitleZoomEnabled {
-            return model.textWidth*model.titleCurrentZoomScale
-        } else {
-            return model.textWidth
-        }
-    }
-
-}
 
 // MARK: - SegmentedViewDelegate
 extension ViewController: SegmentedViewDelegate {
