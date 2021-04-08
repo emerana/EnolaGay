00e2f6e7aa720ffe490f905ac2bf22e32b4fdd81
醉翁之意 <Judy_u@163.com>
2021-04-08 17:02:06 +0800

    优化键盘弹出

--- Example/EnolaGay/键盘优化/KeyBoardPopUpViewCtrl.swift
+++ Example/EnolaGay/键盘优化/KeyBoardPopUpViewCtrl.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
+/// 实现像微信的聊天输入框跟随键盘方案。
 class KeyBoardPopUpViewCtrl: UIViewController {
     
+    /// 需要跟随键盘移动的目标 View。
     @IBOutlet weak var keyBaordView: UIView!
     @IBOutlet weak var textFeild: UITextField!
+    
+    private var keyBoardHeight: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +26,7 @@ class KeyBoardPopUpViewCtrl: UIViewController {
         
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTouches(sender:)))
         tapGestureRecognizer.cancelsTouchesInView = false
-        self.view.addGestureRecognizer(tapGestureRecognizer)
+        view.addGestureRecognizer(tapGestureRecognizer)
         
         NotificationCenter.default.addObserver(self, selector:#selector(keyBoardShowHideAction(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector:#selector(keyBoardShowHideAction(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -31,42 +35,37 @@ class KeyBoardPopUpViewCtrl: UIViewController {
         
     @objc func keyBoardShowHideAction(notification: NSNotification) {
         
-        let userInfo  = notification.userInfo! as NSDictionary
-        let duration = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
-
-        let animations:(() -> Void) = {
-            
+        guard let userInfo = notification.userInfo,
+              let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
+        else { return }
+        
+        let animations: (() -> Void) = { [weak self] in
+            // 键盘弹出过程。
             if notification.name == UIResponder.keyboardWillShowNotification {
-                
-                let keyBoardBounds = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-                _ = self.view.convert(keyBoardBounds, to:nil)
-                _ = self.keyBaordView.frame
-                
-                let deltaY = keyBoardBounds.size.height
-                self.keyBaordView.transform = CGAffineTransform(translationX: 0,y: -deltaY)
-
+                // 得到键盘高度。
+                self?.keyBoardHeight = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect).size.height
+                self?.keyBaordView.transform = CGAffineTransform(translationX: 0,y: -self!.keyBoardHeight)
             }
-
+            // 键盘收起过程。
             if notification.name == UIResponder.keyboardWillHideNotification {
-                self.keyBaordView.transform = CGAffineTransform.identity
+                self?.keyBaordView.transform = CGAffineTransform.identity
             }
-
         }
         
-
+        // 键盘弹出过程时长。
         if duration > 0 {
             let options = UIView.AnimationOptions(rawValue: userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt)
-            UIView.animate(withDuration: duration, delay: 0, options:options, animations: animations, completion: nil)
+            UIView.animate(withDuration: duration, delay: 0, options: options, animations: animations, completion: nil)
         } else {
+            // 键盘已经弹出，只是切换键盘。
             animations()
         }
-
+        
     }
     
-    
     @objc func handleTouches(sender: UITapGestureRecognizer) {
-        
-        if sender.location(in: self.view).y < self.view.bounds.height - 250 {
+        // 只要点击区域不再键盘范围即收起键盘。
+        if sender.location(in: view).y < view.bounds.height - keyBoardHeight {
             textFeild.resignFirstResponder()
         }
     }
