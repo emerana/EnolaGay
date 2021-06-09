f67acbdfeb7991de9f1e44eb3f634a878131a7f9
醉翁之意 <Judy_u@163.com>
2021-06-09 16:25:30 +0800

    tableView 化视频播放器

--- Example/EnolaGay/MyPlayer/MyPlayerViewCtrl.swift
+++ Example/EnolaGay/MyPlayer/MyPlayerViewCtrl.swift
@@ -8,82 +8,64 @@
 
 import UIKit
 import EnolaGay
-import Player
+import SwiftyJSON
 
-class MyPlayerViewCtrl: JudyBaseViewCtrl {
+class MyPlayerViewCtrl: JudyBaseTableViewCtrl {
     override var viewTitle: String? { "Player" }
 
-    fileprivate var player = Player()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.player.playerDelegate = self
-        self.player.playbackDelegate = self
-        self.player.view.frame = self.view.bounds
+        setDataSoruce()
+    }
+
+    override func registerReuseComponents() {
+        let nib = UINib(nibName: "VideoCell", bundle: nil)
+        tableView?.register(nib, forCellReuseIdentifier: "Cell")
+    }
+
+}
+
+extension MyPlayerViewCtrl {
+    
+    func setDataSoruce() {
+        dataSource = [
+            JSON(["urls": "https://video.jingmaiwang.com/smallvideo/-1_20210414135329.mp4"]),
+            JSON(["urls": "https://video.jingmaiwang.com/smallvideo/-1_20210425153032.mp4"]),
+            JSON(["urls": "https://video.jingmaiwang.com/smallvideo/-1_20210422095345.mp4"]),
+            JSON(["urls": "https://video.jingmaiwang.com/smallvideo/-1_20210422100200.mp4"]),
+            JSON(["urls": "https://video.jingmaiwang.com/smallvideo/-1_20210422100512.mp4"]),
+            JSON(["urls": "https://video.jingmaiwang.com/smallvideo/-1_20210410094203.mp4"]),
+            JSON(["urls": "https://video.jingmaiwang.com/smallvideo/-1_20210429131045.mp4"]),
+            JSON(["urls": "https://video.jingmaiwang.com/smallvideo/-1_20210414135138.mp4"]),
+            JSON(["urls": "https://video.jingmaiwang.com/smallvideo/-1_20210415143308.mp4"]),
+            JSON(["urls": "https://video.jingmaiwang.com/smallvideo/-1_20210425160443.mp4"]),
+        ]
+    }
+}
+
+extension MyPlayerViewCtrl {
+    // 明确预估高度。
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        // viewDidLoad 中执行 tableView?.scrollToRow 后，
+        // 导致上下切换 Cell 还会残留上一个 Cell 问题，
+        // 是因为 Estimate 为 Automatic，给其设置一个值即可。
+        return tableView.frame.height
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.frame.height
+    }
+    
+    /// 询问指定 indexPath 的 cell 实例，默认取 identifier 为 cell 的实例。
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)  as! VideoCell
+        cell.json = dataSource[indexPath.row]
+        cell.selectionStyle = .none
         
-        self.player.url = URL(string: "https://video.jingmaiwang.com/smallvideo/-1_20210415143308.mp4")!
-
-        self.addChild(self.player)
-        self.view.addSubview(self.player.view)
-        self.player.didMove(toParent: self)
+        return cell
     }
 
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destinationViewController.
-         // Pass the selected object to the new view controller.
-     }
-     */
-    
+
 }
-
-
-// MARK: - PlayerDelegate
-
-extension MyPlayerViewCtrl: PlayerDelegate {
-    
-    func playerReady(_ player: Player) {
-        print("\(#function) ready")
-    }
-    
-    func playerPlaybackStateDidChange(_ player: Player) {
-        print("\(#function) \(player.playbackState.description)")
-    }
-    
-    func playerBufferingStateDidChange(_ player: Player) {
-    }
-    
-    func playerBufferTimeDidChange(_ bufferTime: Double) {
-    }
-    
-    func player(_ player: Player, didFailWithError error: Error?) {
-        print("\(#function) error.description")
-    }
-    
-}
-
-// MARK: - PlayerPlaybackDelegate
-
-extension MyPlayerViewCtrl: PlayerPlaybackDelegate {
-    
-    func playerCurrentTimeDidChange(_ player: Player) {
-    }
-    
-    func playerPlaybackWillStartFromBeginning(_ player: Player) {
-    }
-    
-    func playerPlaybackDidEnd(_ player: Player) {
-    }
-    
-    func playerPlaybackWillLoop(_ player: Player) {
-    }
-
-    func playerPlaybackDidLoop(_ player: Player) {
-    }
-}
-
---
+++ Example/EnolaGay/MyPlayer/VideoCell.swift
@@ -0,0 +1,106 @@
+//
+//  VideoCell.swift
+//  EnolaGay_Example
+//
+//  Created by 王仁洁 on 2021/6/9.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import EnolaGay
+import Player
+
+class VideoCell: JudyBaseTableCell {
+    
+    fileprivate var player = Player()
+
+    /// 播放按钮。
+    @IBOutlet weak var playerButton: JudyBaseButton!
+    
+    // MARK: - var property
+    
+    
+    // MARK: - life cycle
+    
+    // cell 准备重用时执行的方法。
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        // 此处应重置 cell 状态，清除在重用池里面设置的值。
+    }
+    
+    /// 从 xib 或故事板创建对象将会执行此初始函数。
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.player.playerDelegate = self
+        self.player.playbackDelegate = self
+        self.player.view.frame = contentView.bounds
+        
+        //        self.addChild(self.player)
+        self.contentView.addSubview(self.player.view)
+        // self.player.didMove(toParent: self)
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    
+    // MARK: - override
+    
+    /// 设置数据源事件。
+    override func jsonDidSetAction() {
+        self.player.url = URL(string: json["urls"].stringValue)!
+    }
+    
+}
+
+
+// MARK: - PlayerDelegate
+
+extension VideoCell: PlayerDelegate {
+    
+    func playerReady(_ player: Player) {
+        print("\(#function) ready")
+        self.player.playFromBeginning()
+    }
+    
+    func playerPlaybackStateDidChange(_ player: Player) {
+        print("\(#function) \(player.playbackState.description)")
+    }
+    
+    func playerBufferingStateDidChange(_ player: Player) {
+    }
+    
+    func playerBufferTimeDidChange(_ bufferTime: Double) {
+    }
+    
+    func player(_ player: Player, didFailWithError error: Error?) {
+        print("\(#function) error.description")
+    }
+    
+}
+
+// MARK: - PlayerPlaybackDelegate
+
+extension VideoCell: PlayerPlaybackDelegate {
+    
+    func playerCurrentTimeDidChange(_ player: Player) {
+    }
+    
+    func playerPlaybackWillStartFromBeginning(_ player: Player) {
+    }
+    
+    func playerPlaybackDidEnd(_ player: Player) {
+    }
+    
+    func playerPlaybackWillLoop(_ player: Player) {
+    }
+
+    func playerPlaybackDidLoop(_ player: Player) {
+    }
+}
+
