f361f72e6eeff32148927afeff3f7984fc2eb64b
醉翁之意 <Judy_u@163.com>
2021-07-09 20:17:31 +0800

    定投计算界面配置

---
+++ Example/EnolaGay/CastSurelyCalculation/CastSurelyCalculationViewCtrl.swift
@@ -0,0 +1,50 @@
+//
+//  CastSurelyCalculationViewCtrl.swift
+//  EnolaGay_Example
+//
+//  Created by 醉翁之意 on 2021/7/9.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import EnolaGay
+
+class CastSurelyCalculationViewCtrl: JudyBaseViewCtrl {
+    override var viewTitle: String? { "定投计算" }
+
+    // MARK: - let property and IBOutlet
+    
+    // MARK: - public var property
+
+    // MARK: - private var property
+    
+    // MARK: - life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - override
+    
+    // MARK: - event response
+
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destinationViewController.
+         // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
+
+// MARK: - private methods
+
+private extension CastSurelyCalculationViewCtrl {
+
+}
