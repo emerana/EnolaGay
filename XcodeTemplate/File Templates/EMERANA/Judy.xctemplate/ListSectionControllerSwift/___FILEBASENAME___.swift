c64521477dea8b9a0b9e9e83e5d94f89527442cf
醉翁之意 <Judy_u@163.com>
2021-08-05 09:41:23 +0800

    优化lineWidthOfCell注释

--- XcodeTemplate/File Templates/EMERANA/Judy.xctemplate/ListSectionControllerSwift/___FILEBASENAME___.swift
+++ XcodeTemplate/File Templates/EMERANA/Judy.xctemplate/ListSectionControllerSwift/___FILEBASENAME___.swift
@@ -35,8 +35,10 @@ extension ___FILEBASENAMEASIDENTIFIER___ {
         /// Cell 参与计算的边长，初值为 Line 的长度（包含间距）。
         ///
         /// 一个 Line 中需要显示的所有 Cell 宽度（或高度）及他们之间所有间距的总和，以此来确定单个 Cell 的边长。
-        /// - Warning: 请注意在此处减去不参与计算 Cell 边长的部分，比如整个 collectionView 的内边距。
-        var lineWidthOfCell: CGFloat = collectionView.frame.width // - itemSpacing*2
+        /// - Warning: 请注意在此处减去不参与计算 cell 边长的部分，比如 collectionView.contentInset 的两边。
+        var lineWidthOfCell = collectionView.frame.width
+        // var lineWidthOfCell = collectionView.frame.width - collectionView.contentInset.left - collectionView.contentInset.right
+
         // 正确地计算 cellWidth 公式，若发现实际显示不正确，请确认是否关闭 CollectionView 的 Estimate Size，将其设置为 None.
         lineWidthOfCell = (lineWidthOfCell + itemSpacing)/countOfCells - itemSpacing
         
