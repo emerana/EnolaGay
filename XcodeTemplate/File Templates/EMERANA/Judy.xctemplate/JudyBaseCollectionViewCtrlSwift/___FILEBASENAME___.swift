e0a79ad1ed1aed2dd44fab7fb859147130df68c7
醉翁之意 <Judy_u@163.com>
2021-08-11 15:29:17 +0800

    优化文件模板句号注释。

--- XcodeTemplate/File Templates/EMERANA/Judy.xctemplate/JudyBaseCollectionViewCtrlSwift/___FILEBASENAME___.swift
+++ XcodeTemplate/File Templates/EMERANA/Judy.xctemplate/JudyBaseCollectionViewCtrlSwift/___FILEBASENAME___.swift
@@ -79,6 +79,7 @@ extension ___FILEBASENAMEASIDENTIFIER___ {
         /// 在一个 line 中需要显示的 cell 数量
         let countOfCells: CGFloat = <#3#>
         /// cell 参与计算的边长，初值为 line 的长度（包含间距）
+        /// 
         /// 一个 line 中需要显示的所有 cell. 宽度（或高度）及他们之间所有间距的总和，以此来确定单个 cell 的边长
         /// - Warning: 请注意在此处减去不参与计算 cell 边长的部分，比如 collectionView.contentInset 的两边
         var lineWidthOfCell = collectionView.frame.width
