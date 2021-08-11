f3c8668da16d78f1e57d88e680d813b86621444f
醉翁之意 <Judy_u@163.com>
2021-08-11 15:15:47 +0800

    ListSectionController模板优化

--- XcodeTemplate/File Templates/EMERANA/Judy.xctemplate/ListSectionControllerSwift/___FILEBASENAME___.swift
+++ XcodeTemplate/File Templates/EMERANA/Judy.xctemplate/ListSectionControllerSwift/___FILEBASENAME___.swift
@@ -33,11 +33,8 @@ extension ___FILEBASENAMEASIDENTIFIER___ {
         /// 在一个 line 中需要显示的 cell 数量
         let countOfCells: CGFloat = <#3#>
         /// cell 参与计算的边长，初值为 line 的长度（包含间距）
-        ///
         /// 一个 line 中需要显示的所有 cell. 宽度（或高度）及他们之间所有间距的总和，以此来确定单个 cell 的边长
-
         /// - Warning: 请注意在此处减去不参与计算 cell 边长的部分，比如 collectionView.contentInset 的两边
-
         var lineWidthOfCell = collectionView.frame.width
         // var lineWidthOfCell = collectionView.frame.width - collectionView.contentInset.left - collectionView.contentInset.right
         // 正确地计算 cellWidth 公式，若发现实际显示不正确，请确认是否关闭 collectionView 的 Estimate Size，将其设置为 None.
