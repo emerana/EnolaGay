a69e4f337f7313c40ed239dd07eb63dd196d279b
醉翁之意 <Judy_u@163.com>
2021-09-01 15:04:04 +0800

    加入AGUL！

---
+++ Example/AGUL/Chat/ChatViewCtrl.h
@@ -0,0 +1,13 @@
+//
+//  ChatViewCtrl.h
+//  UUChatTableView
+//
+//  Created by shake on 15/1/4.
+//  Copyright (c) 2015年 uyiuyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatViewCtrl : UIViewController
+
+@end
--- Example/AGUL/ViewController.swift
+++ Example/AGUL/ViewController.swift
@@ -12,9 +12,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         FuckerCtrl.judy.star()
     }
-    
     
 }
 
