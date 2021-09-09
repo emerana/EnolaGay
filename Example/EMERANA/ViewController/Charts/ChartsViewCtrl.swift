a1b2bd9db2f40c95015eb36596bcf1d211fd984d
醉翁之意 <Judy_u@163.com>
2021-09-09 22:05:09 +0800

    isHideWaitingHUD、isHideNotApiHUD 优化

--- Example/EMERANA/ViewController/Charts/ChartsViewCtrl.swift
+++ Example/EMERANA/ViewController/Charts/ChartsViewCtrl.swift
@@ -58,15 +58,15 @@ class ChartsViewCtrl: JudyBaseTableViewCtrl {
     
     // MARK: - Method - 私有方法的代码尽量抽取创建公共class。
 
-    /*
      // MARK: - Navigation
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
          // Get the new view controller using segue.destinationViewController.
          // Pass the selected object to the new view controller.
+        let viewCtrl = segue.destination
+        viewCtrl.modalPresentationStyle = .none
      }
-     */
     
 }
 
