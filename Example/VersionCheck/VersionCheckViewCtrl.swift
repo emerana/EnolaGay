a9c69c736f9e5ef144b944bc84c37c332a65c770
醉翁之意 <Judy_u@163.com>
2021-09-14 13:37:51 +0800

    MVVM功能逻辑分离

--- Example/VersionCheck/VersionCheckViewCtrl.swift
+++ Example/VersionCheck/VersionCheckViewCtrl.swift
@@ -51,16 +51,16 @@ class VersionCheckViewCtrl: JudyBaseViewCtrl {
                 self.queryButton.isHidden = true
                 self.view.endEditing(true)
                 
-                self.viewModel.versionCheck(observable: { (versionStatus, force) in
+                self.viewModel.versionCheck(observable: { (versionInfo, force) in
                     self.queryButton.isHidden = false
 
-                    Judy.log("查询到的 versionStatus：\(versionStatus)")
+                    Judy.log("查询到的 versionStatus：\(versionInfo)")
                     Judy.log("查询强制更新响应的 isHot = \(force)")
 
                     var infoString = "查询结果\n"
                     infoString += "Bundle ID: \(try! self.viewModel.bundleID.value())\n"
                     infoString += "Version: \(try! self.viewModel.version.value())\n"
-                    infoString += versionStatus.0.rawValue
+                    infoString += versionInfo.0.rawValue
 
                     self.infoLabel.text = infoString
                     let highlightedColor = UIColor.darkText
@@ -70,12 +70,12 @@ class VersionCheckViewCtrl: JudyBaseViewCtrl {
                     self.infoLabel.judy.setHighlighted(text: "Version:", color: highlightedColor, font: highlightedFont)
 
                     /// 只有要求强制更新且有新版本的时候弹出强制更新窗口
-                    if versionStatus.0 == .older && force {
+                    if versionInfo.0 == .older && force {
                         let alertController = UIAlertController(title: "请更新版本",
                                                                 message: nil,
                                                                 preferredStyle: .alert)
                         let okAction = UIAlertAction(title: "去更新", style: .destructive, handler: {(_ action: UIAlertAction) -> Void in
-                            if let url = URL(string: versionStatus.1 ?? "") {
+                            if let url = URL(string: versionInfo.1 ?? "") {
                                 if UIApplication.shared.canOpenURL(url) {
                                     Judy.logHappy("正在打开：\(url)")
                                     UIApplication.shared.open(url, completionHandler: nil)
