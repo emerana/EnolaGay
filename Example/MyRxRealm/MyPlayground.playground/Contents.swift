e52de47428b8682f454f74468c711260d6e47580
醉翁之意 <Judy_u@163.com>
2022-11-17 17:31:21 +0800

    Merge branch 'develop' into Test
    # Conflicts:
    #	Example/EnolaGay.xcodeproj/project.pbxproj
    #	Example/MyRxRealm/Base.lproj/Main.storyboard
    #	Example/MyRxRealm/ViewController.swift
    #	Example/Podfile.lock
    #	Example/Pods/Manifest.lock
    #	Example/Pods/Pods.xcodeproj/project.pbxproj

---
+++ Example/MyRxRealm/MyPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+import Foundation
+import EnolaGay
+//import UIKit
+//import RxSwift
+//import RxCocoa
+//import RealmSwift
+//
+//var config = Realm.Configuration.defaultConfiguration
+//
+//let username = "TestEnolaGay"
+//config.fileURL!.deleteLastPathComponent()
+//config.fileURL!.appendPathComponent(username)
+//config.fileURL!.appendPathExtension("realm")
+//
+//let realm = try! Realm(configuration: config)
+//// 组操作
+//
+//let accounts = realm.objects(Account.self)
+//let groups = realm.objects(Group.self)
+//
+//let account = accounts.first
+//try! realm.write {
+//    if let indexAccount = account?.group.first?.accounts.firstIndex(of: account!) {
+//        account?.group.first?.accounts.remove(at: indexAccount)
+//        Judy.log(indexAccount)
+//    }
+//}
+let date = Date()
+Judy.log(date)
+Judy.logl(date)
+Judy.logTime(date)
+Judy.logs(date)
+Judy.logn(date)
+Judy.logt(date)
+Judy.logHappy(date)
+Judy.logWarning(date)
+
