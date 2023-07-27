2303606e71138212161b011de9838b5412a8edea
王仁洁 <Judy_u@163.com>
2023-07-27 17:50:43 +0800

    小修复

--- Example/Pods/Target Support Files/Pods-RefreshAdapter/Pods-RefreshAdapter-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-RefreshAdapter/Pods-RefreshAdapter-frameworks.sh
@@ -176,13 +176,13 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/EnolaGay-iOS11.0/EnolaGay.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/EnolaGay-iOS11.0/EnolaGay.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
